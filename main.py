7e055774a42444eeb6cf45d17e658b7a15794944
Sadeghi <sadeghi85@hotmail.com>
2023-06-26 18:34:48 +0330

    update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ pattern_subscribe = (
 pattern_ss = r"(?<!\w)(ss://[^\s<>]+)"
 pattern_trojan = r"(?<!\w)(trojan://[^\s<>]+)"
 pattern_vmess = r"(?<!\w)(vmess://[^\s<>]+)"
-pattern_vless = r"(?<!\w)(vless://(?:(?!reality)[^\s<>])+)"
+pattern_vless = r"(?<!\w)(vless://(?:(?!=reality)[^\s<>])+(?=[\s<>]))"
 pattern_reality = r"(?<!\w)(vless://[^\s<>]+?security=reality[^\s<>]*)"
 
 array_subscribe = []
